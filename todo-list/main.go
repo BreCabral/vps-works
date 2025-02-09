7abb92a650354be47d6622aa04c7cd6312020ef3
Breno <breno.ara.cabral@gmail.com>
2025-02-09 16:50:16 -0300

    feat: add `todo-list` docker config and golang example

---
+++ todo-list/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type task struct {
+	ID   string `json:"id"`
+	Task string `json:"task"`
+}
+
+var tasks = []task{
+	{ID: "1", Task: "Ola Mundo! Tarefa 1"},
+	{ID: "2", Task: "Ola Mundo! Tarefa 2"},
+	{ID: "3", Task: "Ola Mundo! Tarefa 3"},
+}
+
+func getTasks(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, tasks)
+}
+
+func postTask(c *gin.Context) {
+	var newTask task
+	if err := c.BindJSON(&newTask); err != nil {
+		c.IndentedJSON(http.StatusBadRequest, nil)
+		return
+	}
+
+	tasks = append(tasks, newTask)
+	c.IndentedJSON(http.StatusCreated, newTask)
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/list", getTasks)
+	router.POST("/add_task", postTask)
+
+	router.Run(":8080")
+}
