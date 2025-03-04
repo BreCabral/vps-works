ce339e3ba6c5877fb63197c146b58307113505ff
Breno <breno.ara.cabral@gmail.com>
2025-03-04 18:10:10 -0300

    feat: creates `UserModel` and `tb_users`

---
+++ user-auth/src/main/java/com/brenocabral/userauth/User/UserModel.java
@@ -0,0 +1,26 @@
+package com.brenocabral.userauth.User;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "tb_users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+}
